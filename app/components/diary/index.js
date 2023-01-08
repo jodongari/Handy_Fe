a96133e789f05f8629f9086756c2f0bd11c54455
macho <macho@gmarket.com>
2023-01-08 15:13:38 +0900

    diary index 구현 firebase 연동 에러나고 있음

--- app/components/diary/index.js
+++ app/components/diary/index.js
@@ -29,10 +29,64 @@ class DiaryComponent extends Component {
     this.props.dispatch(getDiaries());
   }
 
+  renderDiary = Diaries =>
+    Diaries.documents.map((item, index) => (
+      <TouchableOpacity key={index}>
+        <View style={styles.diaryContainer}>
+          <View style={{height: 160}}>
+            {item.data.imagePath ? (
+              <View style={styles.indexView}>
+                <Text style={{fontSize: 17, fontWeight: 'bold'}}>
+                  # {index + 1}
+                </Text>
+                <Image
+                  source={require('../../assets/images/image.png')}
+                  resizeMethod={'contain'}
+                  style={{width: 20, height: 20}}
+                />
+              </View>
+            ) : (
+              <View style={{paddingTop: 7, paddingLeft: 7}}>
+                <Text style={{fontSize: 17, fontWeight: 'bold'}}>
+                  # {index + 1}
+                </Text>
+              </View>
+            )}
+
+            {item.data.date ? (
+              <View style={styles.dateView}>
+                <Text style={{fontSize: 16, fontWeight: 'bold'}}>Date: </Text>
+                <Text style={{fontSize: 16}}>{item.data.date}</Text>
+              </View>
+            ):null}
+
+            {item.data.title ? (
+              <View style={styles.dateView}>
+                <Text style={{fontSize: 16, fontWeight: 'bold'}}>Title: </Text>
+                <Text style={{fontSize: 16}}>{item.data.title}</Text>
+              </View>
+            ):null}
+
+            {item.data.description ? (
+              <View style={{paddingTop: 7, paddingLeft: 7}}>
+                <Text style={{fontSize: 16, fontWeight: 'bold'}}>
+                  Description:{' '}
+                </Text>
+                <Text style={{fontSize: 16}}>{item.data.description}</Text>
+              </View>
+            ):null}
+
+          </View>
+        </View>
+      </TouchableOpacity>
+    ));
+
   render() {
     return (
       <View>
-        <ScrollView style={{backgroundColor: '#0f0f0f'}} />
+        <ScrollView style={{backgroundColor: '#0f0f0f'}}>
+          {this.renderDiary(this.props.Diaries)}
+        </ScrollView>
         <TouchableOpacity
           style={{
             position: 'absolute',
@@ -55,7 +109,31 @@ class DiaryComponent extends Component {
   }
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  diaryContainer: {
+    backgroundColor: '#fff',
+    margin: 10,
+    shadowColor: '#cccccc',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    borderRadius: 2,
+  },
+  indexView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 7,
+    paddingLeft: 7,
+    paddingRight: 12,
+    alignItems: 'center',
+  },
+  dateView: {
+    flexDirection: 'row',
+    alignItems: 'baseline',
+    paddingTop: 7,
+    paddingLeft: 7,
+  },
+});
 
 function mapStateToProps(state) {
   return {
